d6f58bc4eb2a1e41478bc2673629625d5e0afc30
Jose Miguel Ortiz C <jose.1701911606@ucaldas.edu.co>
2020-12-10 14:34:50 -0500

    Se reconoce la extension del archivo

--- js/app.js
+++ js/app.js
@@ -1,38 +1,37 @@
-import Helpers from './helpers.js';
+import Helpers from "./helpers.js";
 
-document.addEventListener("DOMContentLoaded", e => {
+document.addEventListener("DOMContentLoaded", (e) => {
+  console.info("GO!!!");
 
-    console.info("GO!!!");
+  document.querySelector("#file-button").addEventListener("click", (e) => {
+    document.querySelector("#file-load").click();
+  });
 
-    document.querySelector('#file-button').addEventListener('click', e => {
-        document.querySelector('#file-load').click();
-    });
+  document.querySelector("#file-load").addEventListener("change", async (e) => {
+    if (e.target.value) {
+      let filePath = e.target.value;
+      let fileExtension = filePath.substr(filePath.length - 4, 4);
 
-    document.querySelector('#file-load').addEventListener('change', async (e) => {
+      if (fileExtension === ".chm") {
+        document.querySelector("#file-name").innerText = e.target.value.match(
+          /[\/\\]([\w\d\s\.\-\(\)]+)$/
+        )[1];
 
-        if(e.target.value){
+        let file = e.target.files[0];
 
-            document.querySelector('#file-name').innerText= e.target.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
-
-            let file = e.target.files[0];
-
-            await Helpers.organizeText(file).then(
-                (code) => {
-                    console.log('complete');
-                    console.log(code);
-                }
-            ).catch(
-                err => { console.log(`Error: ${err}`) }
-            );
-            
-        } 
-        else {
-
-            document.querySelector('#file-name').innerText= `No file`;
-        }
-
-    });
-    
-
+        await Helpers.organizeText(file)
+          .then((code) => {
+            console.log("complete");
+            console.log(code);
+          })
+          .catch((err) => {
+            console.log(`Error: ${err}`);
+          });
+      } else {
+        alert("Tipo de archivo invalido");
+      }
+    } else {
+      document.querySelector("#file-name").innerText = `No file`;
+    }
+  });
 });
-
